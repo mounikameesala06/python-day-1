04f659e02a59c722e6183aecbdfb99a82d4c0943
mounikameesala06 <mounikameesala06@gmail.com>
2025-06-26 15:07:44 +0530

    Add files via upload

---
+++ python day-1.py
@@ -0,0 +1,39 @@
+a=10
+print(a)
+print (type(a))
+print(id(a))
+
+a1=-90
+print(a1)
+print(type(a1))
+print(id(a1))
+
+b=897.23
+print(b)
+print(type(b))
+print (id(b))
+
+a=-98.29
+print(a)
+print(type(a))
+print (id(a))
+
+c=98+23j
+print(c)
+print(type(c))
+print(id(c))
+
+k=-87+23j
+print(k)
+print(type(k))
+print(id(k))
+
+a="ISTS"
+print(a)
+print(type(a))
+print(id(a))
+
+b="mouni"
+print(b)
+print(type(b))
+print(id(b))
